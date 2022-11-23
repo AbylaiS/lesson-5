18df3bd3e4e873166f6b1ae4a75b2419add4c69c
AbylaiS <iridium.channel@gmail.com>
2022-11-23 21:33:18 +0300

    Lesson 5

---
+++ src/Main.java
@@ -0,0 +1,138 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+     /*  if (20 > 12){                         //Урок 5
+           System.out.println("20 больше 12");
+       }
+       int x = 35;
+       int y = 15;
+       if (x > y);{
+            System.out.println("x больше y");
+        }
+        if (10 < 5){
+            System.out.println("10 меньше 5");
+        }
+        else {
+            System.out.println("10 больше 5");
+        }
+        int day = 35;
+        if(day < 27){
+            System.out.println("Понидельник");
+        }
+        else{
+            System.out.println("Среда");
+        }
+        if (day < 22){
+            System.out.println("Вторник");
+        }
+        else if (day < 33){
+            System.out.println("Четверг");
+        }
+        else{
+            System.out.println("Пятница");
+        }
+        String resault = (day <11)?"Суббота!" : "Воскресенье" ;
+        System.out.println(resault);*/
+
+        /* int month = 7;
+        switch (month) {
+            case 1: {
+                System.out.println("January");
+                break;
+            }
+            case 2: {
+                System.out.println("February");
+                break;
+            }
+            case 3: {
+                System.out.println("March");
+                break;
+            }
+            case 4: {
+                System.out.println("April");
+                break;
+            }
+            case 5: {
+                System.out.println("May");
+            }
+            case 6: {
+                System.out.println("June");
+                break;
+            }
+            case 7: {
+                System.out.println("July");
+                break;
+            }
+            default:
+                System.out.println("September");
+
+        } */
+
+       /* int i = 0;
+        while (i < 6) {
+            System.out.println(i);
+            i++;
+        }*/
+        /*int x = 0;
+        do {
+            System.out.println(x);
+            x++;
+        }while(x < 6);
+        */
+
+       /* for(int i = 0; i < 6; i++){
+            System.out.println(i);
+        }*/
+       /* for (int i = 0; i <= 10; i = i + 2){
+            System.out.println(i);
+        }
+        */
+        /*for(int i = 0; i <= 10;  i = i + 2){
+            if (i == 4) {
+                break;
+            }
+            System.out.println(i);
+        }*/
+      /*  for(int i = 0; i <= 10;  i = i + 2) {
+            if (i == 4) {
+                continue;
+            }
+            System.out.println(i);
+        }*/
+
+        /*int i = 0;
+        while (i < 10){
+            if (i == 4){
+                i++;
+                continue;
+            }
+            System.out.println(i);
+            i++;
+        }*/ //Домашнее задание
+        /*int x = 15;
+        int y = 20;
+        if (x == 20) {
+            System.out.println("x");
+        } else {
+            System.out.println("y");
+        }*/
+       /* int a = 5000;
+        if (a < 5000){
+            System.out.println(""); //Не понял как решить эту задачу
+        }
+        else {
+            System.out.println("Скидки нет");
+        }*/
+       /* int i = 5;
+        while (i > 0) {
+            System.out.println(i);
+            i--;
+        }*/
+        int a = 1;
+        while (a < 0){
+            System.out.println(1);
+            a++;
+        }
+    }
+}
